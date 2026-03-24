c502a48bc37f52dac65483f3f770dd4ab35a885b
Detellez <164583101+Detellez@users.noreply.github.com>
2026-03-24 18:07:09 -0400

    Update content_auth.js

--- public/content_auth.js
+++ public/content_auth.js
@@ -631,20 +631,7 @@
     const CEREBRO_URL = 'https://script.google.com/macros/s/AKfycbwmvvUWJhV8QkfrVjKJ0MMJAL9rdzJ3jFkSb-k5z9hzDpnwC5wiawPUesmQ2osMCwM/exec';
     const FIREBASE_URL = "https://sst-notificaciones-carmen-default-rtdb.firebaseio.com/alerta_activa.json";
     
-    // URLs de los Obreros Activos
-    const OBRERO_MEXICO_URL = 'https://script.google.com/macros/s/AKfycbwmvvUWJhV8QkfrVjKJ0MMJAL9rdzJ3jFkSb-k5z9hzDpnwC5wiawPUesmQ2osMCwM/exec';
-    const OBRERO_PERU_URL = 'https://script.google.com/macros/s/AKfycbwmvvUWJhV8QkfrVjKJ0MMJAL9rdzJ3jFkSb-k5z9hzDpnwC5wiawPUesmQ2osMCwM/exec';
-    const OBRERO_BRASIL_URL = 'https://script.google.com/macros/s/AKfycbwmvvUWJhV8QkfrVjKJ0MMJAL9rdzJ3jFkSb-k5z9hzDpnwC5wiawPUesmQ2osMCwM/exec';
-    const OBRERO_COLOMBIA_URL = 'https://script.google.com/macros/s/AKfycbwmvvUWJhV8QkfrVjKJ0MMJAL9rdzJ3jFkSb-k5z9hzDpnwC5wiawPUesmQ2osMCwM/exec';
-
-    const API_URL = (function() {
-        const href = window.location.href;
-        if (href.includes('mx-crm.certislink.com') || href.includes('variousplan.com')) return OBRERO_MEXICO_URL;
-        if (href.includes('pe-crm.certislink.com')) return OBRERO_PERU_URL;   
-        if (href.includes('creddireto.com')) return OBRERO_BRASIL_URL;
-        if (href.includes('co-crm.certislink.com')) return OBRERO_COLOMBIA_URL;
-        return CEREBRO_URL;            
-    })();
+    const API_URL = CEREBRO_URL;
     
     // Variable para detener intervalos
     let isExtensionAlive = true;
